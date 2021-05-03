0fd8e4756d2be796e46abd199ee169a59b48f905
axiomattik <axiomattik@pocketgecko.co.uk>
2021-05-03 12:49:02 +0100

    css and js support

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-const { series, parallel } = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
+const path = require('path');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
 const run = require('gulp-run');
+const sass = require('gulp-sass');
 
 const bs = require('browser-sync').create();
+
 
 function feedDB(cb) {
 	return run(`
@@ -9,6 +14,46 @@ function feedDB(cb) {
 		sqlite3 novels.db < schema.sql; 
 		php dummy-data.php;
 	`).exec()
+}
+
+
+function compileSCSS() {
+	return src(
+			'assets/css/style.scss', 
+			{ 'allowEmpty': true } 
+		).pipe(sass({ outputStyle: 'compressed' })
+		.on('error', sass.logError))
+		.pipe(dest('assets/css/'));
+}
+
+
+function compilePrintSCSS() {
+	return src(
+			'assets/css/print.scss', 
+			{ 'allowEmpty': true } 
+		).pipe(sass({ outputStyle: 'compressed' })
+		.on('error', sass.logError))
+		.pipe(dest('assets/css/'));
+}
+
+
+function minifyCustomJS() {
+	return src(
+			'assets/js/custom/**/*.js',
+			{ 'allowEmpty': true }
+		).pipe(concat('skoveler.js'))
+		.pipe(minify())
+		.pipe(dest('assets/js/.'));
+}
+
+
+function minifyVendorJS() {
+	return src(
+			'assets/js/vendor/**/*.js',
+			{ 'allowEmpty': true }
+		).pipe(concat('vendor.js'))
+		.pipe(minify())
+		.pipe(dest('assets/js/.'));
 }
 
 
@@ -28,4 +73,13 @@ exports.watch = watchFiles;
 
 exports.serve = parallel(exports.watch, browsersync);
 
-exports.default = exports.serve;
+exports.css = series(compileSCSS, compilePrintSCSS);
+
+exports.js = series(minifyCustomJS, minifyVendorJS);
+
+
+exports.default = series(
+	exports.css,
+	exports.js,
+	exports.serve
+);
